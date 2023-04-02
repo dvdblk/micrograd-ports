3651f4f8f7aa234a8bdfe143be30461f41c11bdd
dvdblk <git@dvdblk.com>
2023-04-02 23:43:37 +0200

    Update Engine.swift generics to Double

--- micrograd-swift/Tests/MicrogradSwiftTests/MicrogradSwiftTests.swift
+++ micrograd-swift/Tests/MicrogradSwiftTests/MicrogradSwiftTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class MicrogradSwiftTests: XCTestCase {
     func testValue() throws {
         // Simple comparison
-        XCTAssertEqual(Value<Int>(3), Value(3))
+        XCTAssertEqual(Value(3), Value(3))
         XCTAssertNotEqual(Value(3.0), Value(2))
         
         // implicit typing
@@ -14,13 +14,21 @@ final class MicrogradSwiftTests: XCTestCase {
     }
     
     func testValueToString() throws {
-        XCTAssertEqual(Value(1).description, "1")
+        XCTAssertEqual(Value(1).description, "1.0")
         XCTAssertEqual(Value(1.0).description, "1.0")
-        XCTAssertEqual(Value(-1).description, "-1")
+        XCTAssertEqual(Value(-1).description, "-1.0")
         XCTAssertEqual(Value(-Double(1.0)).description, "-1.0")
     }
     
-    func testValueAddition() throws {
+    func testValueExpressibleByFloatLiteral() throws {
+        XCTAssertEqual(Value(4.0), 4.0)
+    }
+    
+    func testValueAdditionMultiplication() throws {
+        let a = Value(2.0)
+        let b = Value(-3.0)
+        let c = Value(10)
         
+        XCTAssertEqual(a * b + c, 4.0)
     }
 }
